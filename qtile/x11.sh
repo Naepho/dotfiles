4f3572e90bad74e62313fcb774b4a9e3a28d4f57
Naepho <naepho@mail.com>
2022-03-14 21:55:53 +0100

    Login with X11 working

--- qtile/x11.sh
+++ qtile/x11.sh
@@ -1,2 +1,3 @@
-sleep 3
+sleep 1
 xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output HDMI-1-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+numlockx
